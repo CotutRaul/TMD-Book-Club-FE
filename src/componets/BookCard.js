523f3aac903633ad66710cbf7ebfda3c3aa7c25e
Cotut <raul.cotut@endava.com>
2022-07-18 19:27:18 +0300

    BookCard almost perfect

---
+++ src/componets/BookCard.js
@@ -0,0 +1,41 @@
+import React, {useState, useEffect} from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+
+export default function BookCard(props) {
+
+  const [imageUrl, setImageUrl] = useState();
+
+  useEffect(() => {
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=${props.book.title}`, {
+      method: "GET"
+    })
+      .then(res => res.json())
+      .then(jsonResult => setImageUrl(jsonResult.items[0].volumeInfo.imageLinks.thumbnail))
+    }, [])
+
+  
+
+  return (
+    <Card sx={{maxWidth: 200,margin: 1, flex: "1 0 200px"}}>
+      <CardActionArea sx={{height: "100%"}}>
+        {imageUrl &&
+        <CardMedia sx={{ maxHeight: 275 }}
+          component="img"
+          image = {imageUrl}
+        />}
+        <CardContent sx={{height: "100%"}}>
+          <Typography gutterBottom variant="h5" component="div">
+            {props.book.title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {props.book.author}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import NavBar from '../../componets/NavBar'
+import BookCard from '../../componets/BookCard'
+import "./Home.css"
 
 function Home() {
+  const [books, setBooks] = useState(null);
+
+
+  useEffect(() => {
+    fetch(`http://localhost:8080/bookInfos`, {
+      method: "GET"
+    })
+      .then(res => res.json())
+      .then(jsonResult => setBooks(jsonResult))
+  }, [])
+
+  const generateBookCards = () =>{
+
+  }
+
+
   return (
     <div>
-      <NavBar/>
+      <NavBar />
+      <div className='BookList'>
+        {books && books.map((book) =>
+          <BookCard key={book.id} book = {book}/>)}
+      </div>
     </div>
   )
 }
