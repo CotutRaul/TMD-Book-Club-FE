606a71dac0e6f09adb2d4a65bac99db48b1b5bb2
Cotut <raul.cotut@endava.com>
2022-07-27 16:57:03 +0300

    Search bar for home

--- src/componets/BookList.js
+++ src/componets/BookList.js
@@ -7,7 +7,9 @@ function BookList(props) {
     const classes = useStyle()
     return (
         <div className={classes.bookList} >
-            {props.books && props.books.map((book) =>
+            {props.books && props.books
+            .filter((book) => {return props.filter ? book.title.toLowerCase().includes(props.filter.search.toLowerCase()) || book.author.toLowerCase().includes(props.filter.search.toLowerCase()) : true}) 
+            .map((book) =>
                 <BookCard key={book.id} book={book} action={props.action}/>)}
         </div>
     )
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,15 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from 'react'
 import BookList from '../../componets/BookList'
+import TextField from '@mui/material/TextField';
+import { makeStyles } from '@mui/styles'
 import { getAllBookInfo } from '../../services/bookInfoService'
 
 
 function Home() {
   const [books, setBooks] = useState(null);
+  const [search, setSearch] = useState("");
+  const classes = useStyle()
 
   useEffect(() => {
     const fetchData = async () => {
@@ -16,11 +21,29 @@ function Home() {
   }, [])
 
   return (
-
     <div>
-      <BookList books={books}></BookList>
+      
+      <div>
+        <TextField
+          id="searchField"
+          className={classes.searchField}
+          label="Search"
+          type="text"
+          onChange={e => setSearch(e.target.value)}
+        />
+      </div>
+      <BookList books={books} filter={{ search: search }}></BookList>
     </div>
   )
 }
+
+const useStyle = makeStyles({
+  searchField: {
+    marginTop: "20px",
+    marginBottom: "10px",
+    marginLeft: "5px",
+    width: "300px"
+  }
+})
 
 export default Home
